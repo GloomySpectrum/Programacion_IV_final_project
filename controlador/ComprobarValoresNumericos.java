a02fad29905744b84ab25a10d9cc6d712245d9a4
GloomySpectrum <147014076+GloomySpectrum@users.noreply.github.com>
2024-06-10 14:22:51 -0500

    Update ComprobarValoresNumericos.java

--- controlador/ComprobarValoresNumericos.java
+++ controlador/ComprobarValoresNumericos.java
@@ -1,16 +1,14 @@
 package controlador;
 
 public class ComprobarValoresNumericos {
-	
-	public static boolean esNumero(String numero){
-        
-        try{
-        	Long.parseLong(numero);
-        	
-            return true;
-        }catch(NumberFormatException e) {
-        	return false;
+    
+    public static boolean esNumero(String numero) {
+        try {
+            Long.parseLong(numero); // Intenta convertir la cadena a un número largo (long)
+            return true; // Si tiene éxito, devuelve true
+        } catch (NumberFormatException e) {
+            return false; // Si hay una excepción, devuelve false
         }
     }
-
 }
+
